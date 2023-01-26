6cb487f293957317e8b5f934510d3e5563d77600
Eric_Obeng Nti <nti@fim.uni-passau.de>
2023-01-26 20:28:16 +0100

    The 1st version of the Church APP, no databases Included.

---
+++ index.js
@@ -0,0 +1,67 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("index");
+  res.sendFile(__dirname + "\views\contact.ejs");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.get("/events", function(req, res) {
+  res.render("events");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+app.post("/", function(req, res){
+  console.log(req.body);
+});
+
+app.get("/sermon", function(req, res) {
+  res.render("sermon");
+});
+
+app.get("/ministry", function(req, res) {
+  res.render("ministry");
+});
+
+app.get("/sermon-single", function(req, res) {
+  res.render("sermon-single");
+});
+
+app.get("/blog-single", function(req, res) {
+  res.render("blog-single");
+});
+
+app.get("/churchservices1", function(req, res){
+  res.render("churchservices1")
+})
+
+app.get("/churchservices2", function(req, res){
+  res.render("churchservices2")
+})
+
+app.get("/churchservices3", function(req, res){
+  res.render("churchservices3")
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
